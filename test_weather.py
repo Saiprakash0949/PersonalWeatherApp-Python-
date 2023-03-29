91575eddadf438f5c49657b75bc07a348d7ca90d
saii_ch_ <127693953+Saiprakash0949@users.noreply.github.com>
2023-03-29 16:30:14 +0200

    Update test_weather.py
    Added an output valid data function into the test script

--- test_weather.py
+++ test_weather.py
@@ -15,21 +15,27 @@ class TestWeatherApp(unittest.TestCase):
         self.assertIn('sunset_time', weather_data)
 
 
-    def test_format_output_valid_data(self):
+   def test_format_output_valid_data(self):
         # Test with valid weather data
         weather_data = {
             'location': 'London, GB',
             'description': 'Clear',
+            'temperature': 15.0,
+            'humidity': 77,
+            'wind_speed': 3.09,
             'sunrise_time': '06:08:38',
             'sunset_time': '17:48:17'
         }
         expected_output = "Location: London, GB\n" \
                           "Conditions: Clear\n" \
+                          "Temperature: 15.00°C\n" \
+                          "Humidity: 77%\n" \
+                          "Wind Speed: 3.09 m/s\n" \
                           "Sunrise: 06:08:38\n" \
                           "Sunset: 17:48:17"
         output = format_output(weather_data)
         self.assertEqual(output, expected_output)
-
-
+        
+        
 if __name__ == "__main__":
     unittest.main()
